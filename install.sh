c2fd4c76d3c96989d2175205c92c4bd6df6e4182
Andreas Veithen <andreas.veithen@gmail.com>
2016-10-11 18:51:36 +0000

    Add script to create a dev workstation in the cloud

---
+++ install.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+
+set -e
+
+cd
+
+#wget -qO- https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google-chrome.list'
+sudo apt-get update
+sudo apt-get install -y --allow-unauthenticated \
+  metacity light-themes gnome-panel gnome-settings-daemon gnome-terminal tightvncserver \
+  openjdk-8-jdk subversion libsvnclientadapter-java \
+  google-chrome-stable libgnome2-bin
+
+[ -d .vnc ] || mkdir .vnc
+
+cat > .vnc/xstartup <<EOF
+#!/bin/sh
+
+export XKL_XMODMAP_DISABLE=1
+unset SESSION_MANAGER
+unset DBUS_SESSION_BUS_ADDRESS
+
+[ -x /etc/vnc/xstartup ] && exec /etc/vnc/xstartup
+[ -r $HOME/.Xresources ] && xrdb $HOME/.Xresources
+xsetroot -solid grey
+#vncconfig -iconic &
+
+gnome-panel &
+gnome-settings-daemon &
+metacity &
+gnome-terminal &
+EOF
+
+chmod a+x .vnc/xstartup
+
+cat > .vnc/tightvncserver.conf <<EOF
+\$geometry = "2048x1024";
+EOF
+
+[ -d eclipse ] || wget -qO- "http://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/neon/1/eclipse-java-neon-1-linux-gtk-x86_64.tar.gz&r=1" | tar xz
+
+eclipse/eclipse -application org.eclipse.equinox.p2.director -nosplash -repository https://dl.bintray.com/subclipse/releases/subclipse/4.2.x/,http://download.eclipse.org/tools/ajdt/46/dev/update -installIU org.tigris.subversion.subclipse.feature.group,org.eclipse.ajdt.feature.group
+
+[ -d apache-maven-3.3.9 ] || wget -qO- http://www-us.apache.org/dist/maven/maven-3/3.3.9/binaries/apache-maven-3.3.9-bin.tar.gz | tar xz
+[ -d bin ] || mkdir bin
+ln -sf ../apache-maven-3.3.9/bin/mvn bin/mvn
+
+
